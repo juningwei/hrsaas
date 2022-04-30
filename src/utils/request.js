5ca9049524bb464db5b9e075e97fb6873b723015
juningwei <289704828@qq.com>
2022-04-30 18:44:49 +0800

    注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,17 @@ const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 });
-service.interceptors.request.use()
+service.interceptors.request.use(config => {
+  // 在这个位置需要统一的去注入token
+  if (store.getters.token) {
+    // 如果token存在 注入token
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config // 必须返回配置
+}, error => {
+  return Promise.reject(error)
+})
+
 service.interceptors.response.use(response => {
   const {success, message, data } = response.data;
   if (success) {

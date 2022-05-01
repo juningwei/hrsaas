e095cce49929a375dd0a1a4889b041ed71b5ac05
juningwei <289704828@qq.com>
2022-05-01 14:05:00 +0800

    左侧菜单图标

--- src/router/modules/departments.js
+++ src/router/modules/departments.js
@@ -1,5 +1,6 @@
 // 导出属于员工的路由规则
 import Layout from '@/layout'
+import { Icon } from 'element-ui'
 //  {  path: '', component: '' }
 // 每个子模块 其实 都是外层是layout  组件位于layout的二级路由里面
 export default {
@@ -12,7 +13,8 @@ export default {
     component: () => import('@/views/departments'),
     // 路由元信息  其实就是存储数据的对象 我们可以在这里放置一些信息
     meta: {
-      title: '组织架构' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+      title: '组织架构', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+      icon: 'tree'
     }
   }]
 }

7b21b6f4aa521eb1d630513a80317a17c6b45778
juningwei <289704828@qq.com>
2022-05-01 19:08:01 +0800

    点击新增按钮弹窗

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -5,26 +5,30 @@
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
         <!-- 缺少treeNode -->
-        <tree-tools :tree-node="company" :is-root="true"/>
+        <tree-tools :tree-node="company" :is-root="true" @addDepts="addDepts"/>
         <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
           <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
           <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
-          <tree-tools slot-scope="{ data }" :tree-node="data"  @delDepts="getDepartments"/>
+          <tree-tools slot-scope="{ data }" :tree-node="data"  @delDepts="getDepartments" @addDepts="addDepts"/>
         </el-tree>
       </el-card>
+     
     </div>
+     <add-dept :show-dialog="showDialog" />
   </div>
 </template>
 <script>
 import TreeTools from "./components/tree-tools.vue";
 import { getDepartments } from "@/api/departments";
 import { tranListToTreeData } from "@/utils";
+import AddDept from './components/add-dept.vue' // 引入新增部门组件
 
 export default {
   components: {
     TreeTools,
+    AddDept
   },
   data() {
     return {
@@ -41,6 +45,9 @@ export default {
       defaultProps: {
         label: "name", // 表示 从这个属性显示内容
       },
+      showDialog: false, // 显示窗体
+      node: {}
+
     };
   },
   created() {
@@ -54,7 +61,11 @@ export default {
       console.log(result.depts);
       this.departs = tranListToTreeData(result.depts, "");
     },
-    
+    addDepts(node) {
+      this.showDialog = true // 显示弹层
+      // 因为node是当前的点击的部门， 此时这个部门应该记录下来,
+      this.node = node
+    }
   },
 };
 </script>

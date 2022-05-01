01681025a65e77b2e953447aed02d0cc06e04a9a
juningwei <289704828@qq.com>
2022-05-01 12:56:57 +0800

    token失效的被动介入

--- src/utils/request.js
+++ src/utils/request.js
@@ -46,8 +46,14 @@ service.interceptors.response.use(
     }
   },
   (error) => {
-    Message.error(error);
-    return Promise.reject(error);
+    if (error.response && error.response.data && error.response.data.code === 10002) {
+      // 当等于10002的时候 表示 后端告诉我token超时了
+      store.dispatch('user/logout') // 登出action 删除token
+      router.push('/login')
+    } else {
+      Message.error(error.message) // 提示错误信息
+    }
+    return Promise.reject(error)
   }
 );
 

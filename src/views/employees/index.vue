325a73367a98d93daf1402a183569c68452672fd
juningwei <289704828@qq.com>
2022-05-03 14:08:02 +0800

    格式化聘用形式

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -15,7 +15,7 @@
           <el-table-column label="序号" sortable="" type="index" />
           <el-table-column label="姓名" sortable="" prop="username" />
           <el-table-column label="工号" sortable="" prop="workNumber" />
-          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" />
+          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" :formatter="formatEmployment"/>
           <el-table-column label="部门" sortable="" prop="departmentName" />
           <el-table-column label="入职时间" sortable="" prop="timeOfEntry" />
           <el-table-column label="账户状态" sortable="" prop="enableState" />
@@ -47,7 +47,7 @@
 
 <script>
 import { getEmployeeList } from '@/api/employees'
-
+import EmployeeEnum from '@/api/constant/employees'
 export default {
   data() {
     return {
@@ -75,6 +75,11 @@ export default {
       this.page.total = total
       this.list = rows
       this.loading = false
+    },
+    formatEmployment(row, column, cellValue, index) {
+      // 要去找 1所对应的值
+      const obj = EmployeeEnum.hireType.find(item => item.id === cellValue)
+      return obj ? obj.value : '未知'
     }
   }
 }

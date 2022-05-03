94767f4ada5c2935257271408949967c320a0b30
juningwei <289704828@qq.com>
2022-05-03 16:37:00 +0800

    实现excel导入

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -3,13 +3,28 @@
 </template>
 
 <script>
+import {importEmployee} from '@/api/employees'
 export default {
   methods: {
-      success({ header, results }) {
-      console.log(header);
-      console.log(results);
-
-      debugger
+      async success({ header, results }) {
+        console.log(results);
+        const userRelations = {
+          '入职日期': 'timeOfEntry',
+          '手机号': 'mobile',
+          '姓名': 'username',
+          '转正日期': 'correctionTime',
+          '工号': 'workNumber'
+        }
+        const arr = []
+       results.forEach(item => {
+          const userInfo = {}
+          Object.keys(item).forEach(key => {
+            userInfo[userRelations[key]] = item[key]
+          })
+         arr.push(userInfo) 
+        })
+        await importEmployee(arr) // 调用导入接口
+        this.$router.back()
     }
   },
 }

1952270848f09a76821d0252456f2c381cfef5ad
juningwei <289704828@qq.com>
2022-05-03 14:54:25 +0800

    新增员工的表单校验

--- src/api/employees.js
+++ src/api/employees.js
@@ -27,3 +27,13 @@ export function getEmployeeSimple() {
     method: 'delete'
   })
 }
+/** **
+ *  新增员工的接口
+ * **/
+ export function addEmployee(data) {
+  return request({
+    method: 'post',
+    url: '/sys/user',
+    data
+  })
+}

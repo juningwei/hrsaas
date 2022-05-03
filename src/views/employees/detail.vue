a059073fe2010167245db356e3cb2934b984e19b
juningwei <289704828@qq.com>
2022-05-03 20:35:55 +0800

    封装岗位组件

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -21,7 +21,9 @@
           <el-tab-pane label="个人详情" >
             <component :is="userComponent" />
           </el-tab-pane>
-          <el-tab-pane label="岗位信息" />
+          <el-tab-pane label="岗位信息" >
+            <component :is="JobInfo" />
+          </el-tab-pane>
         </el-tabs>
       </el-card>
     </div>
@@ -32,13 +34,17 @@
    import { getUserDetailById } from '@/api/user'
     import { saveUserDetailById } from '@/api/employees'
     import UserInfo from './components/user-info.vue'
+    import JobInfo from './components/job-info.vue'
+
   export default {
     components:{
-      UserInfo
+      UserInfo,
+      JobInfo
     },
   data() {
     return {
       userComponent: 'UserInfo',
+      JobInfo:'JobInfo',
       userId: this.$route.params.id, // 这样可以后面直接通过 this.userId进行获取数据
       userInfo: {
         //   专门存放基本信息

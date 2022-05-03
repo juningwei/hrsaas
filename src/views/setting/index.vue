d767af50f4d349b9aba444dda25140660f09cb46
juningwei <289704828@qq.com>
2022-05-03 13:05:52 +0800

    编辑功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -22,7 +22,7 @@
                <!-- 作用域插槽 -->
           <template slot-scope="{ row }">
                   <el-button size="small" type="success">分配权限</el-button>
-                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button size="small" type="primary" @click="editRole(row.id)" >编辑</el-button>
                   <el-button size="small" type="danger" @click="deleteRole(row.id)">删除</el-button>
           </template>
               </el-table-column>
@@ -64,12 +64,30 @@
 
         </el-tabs>
       </el-card>
+
+      <el-dialog title="编辑弹层" :visible="showDialog" @close="btnCancel">
+      <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="roleForm.name" />
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="roleForm.description" />
+        </el-form-item>
+      </el-form>
+      <!-- 底部 -->
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" @click="btnCancel">取消</el-button>
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
-import { getRoleList , getCompanyInfo, deleteRole} from '@/api/setting.js'
+import { getRoleList , getCompanyInfo, deleteRole, updateRole, getRoleDetail } from '@/api/setting.js'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -85,7 +103,13 @@ export default {
         pagesize: 10,
         total: 0 // 记录总数
       },
-      formData: {}
+      formData: {},
+      showDialog: false,
+      // 专门接收新增或者编辑的编辑的表单数据
+      roleForm: {},
+      rules: {
+        name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }]
+      },
     }
   },
   created() {
@@ -120,6 +144,36 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    async editRole(id) {
+      this.roleForm = await getRoleDetail(id)
+      this.showDialog = true // 为了不出现闪烁的问题 先获取数据 再弹出层
+    },
+    async btnOK() {
+      try {
+        await this.$refs.roleForm.validate()
+        // 只有校验通过的情况下 才会执行await的下方内容
+        // roleForm这个对象有id就是编辑 没有id就是新增
+        if (this.roleForm.id) {
+          await updateRole(this.roleForm)
+        } else {
+          // 新增业务
+        }
+        // 重新拉取数据
+        this.getRoleList()
+        this.$message.success('操作成功')
+        this.showDialog = false
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    btnCancel() {
+      this.$refs.roleForm.reset
+      this.roleForm = {
+        
+      }
+      this.$refs.roleForm.resetFields() // 重置校验字段
+      this.showDialog = false
     }
   }
 }

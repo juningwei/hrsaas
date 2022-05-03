fa58c2e0520a6b3c15a0cf805b1ea8fc0b20fb37
juningwei <289704828@qq.com>
2022-05-03 15:15:07 +0800

    加载部门数据

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -18,7 +18,16 @@
         <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号" />
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门" />
+        <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门" @focus="getDepartments" />
+        <!-- 放置一个tree组件 -->
+        <el-tree
+          v-if="showTree"
+          v-loading="loading"
+          :data="treeData"
+          default-expand-all=""
+          :props="{ label: 'name' }"
+          @node-click="selectNode"
+        />
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
         <el-date-picker v-model="formData.correctionTime" style="width:50%" placeholder="请选择日期" />
@@ -37,6 +46,8 @@
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from "@/utils";
 export default {
   props: {
     showDialog: {
@@ -66,9 +77,23 @@ export default {
         workNumber: [{ required: true, message: '工号不能为空', trigger: 'blur' }],
         departmentName: [{ required: true, message: '部门不能为空', trigger: 'change' }],
         timeOfEntry: [{ required: true, message: '入职时间', trigger: 'blur' }]
-      }
+      },
+             // 表单数据
+       treeData: [], // 定义数组接收树形数据
+       showTree: false, // 控制树形的显示或者隐藏
+       loading: false, // 控制树的显示或者隐藏进度条
     }
   },
+  methods: {
+      async getDepartments() {
+        this.showTree = true
+        this.loading = true
+        const { depts } = await getDepartments()
+        // depts是数组 但不是树形
+        this.treeData = tranListToTreeData(depts, '')
+        this.loading = false
+    }
+  }
 }
 </script>
 

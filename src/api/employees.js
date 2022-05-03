a744f23ec1a9787dd29f6adb29ce7c38030297fa
juningwei <289704828@qq.com>
2022-05-03 14:23:15 +0800

    删除员工功能

--- src/api/employees.js
+++ src/api/employees.js
@@ -17,3 +17,13 @@ export function getEmployeeSimple() {
     params
   })
 }
+/**
+ * 删除员工接口
+ * ****/
+
+ export function delEmployee(id) {
+  return request({
+    url: `/sys/user/${id}`,
+    method: 'delete'
+  })
+}

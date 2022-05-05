9311963cecdc266bb5b1afbc84df0a072e567d8d
juningwei <289704828@qq.com>
2022-05-05 19:28:23 +0800

    获取权限数据并转化为树形

--- src/api/permission.js
+++ src/api/permission.js
@@ -1,3 +1,5 @@
+import request from '@/utils/request'
+
 // 获取权限
 export function getPermissionList(params) {
   return request({
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -8,29 +8,61 @@
         </template>
       </page-tools>
       <!-- 表格 -->
-      <el-table border>
-        <el-table-column align="center" label="名称" />
-        <el-table-column align="center" label="标识" />
-        <el-table-column align="center" label="描述" />
-        <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="text">添加</el-button>
-            <el-button type="text">编辑</el-button>
-            <el-button type="text">删除</el-button>
-          </template>
-        </el-table-column>
-
-      </el-table>
+      <el-table :data="list" border="" row-key="id">
+          <el-table-column label="名称" prop="name" />
+          <el-table-column label="标识" prop="code" />
+          <el-table-column label="描述" prop="description" />
+          <el-table-column label="操作">
+            <template slot-scope="{ row }">
+              <el-button v-if="row.type === 1" type="text" @click="addPermission(row.id, 2)">添加</el-button>
+              <el-button type="text" @click="editPermission(row.id)">编辑</el-button>
+              <el-button type="text" @click="delPermission(row.id)"> 删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils'
 export default {
+data() {
+    return {
+      list: [],
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      },
+      rules: {
+        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+      },
+      showDialog: false
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  computed: {
+    showText() {
+      return this.formData.id ? '编辑' : '新增'
+    }
+  },
+  methods: {
+    async  getPermissionList() {
+      let list = await getPermissionList();
+      this.list = tranListToTreeData(list, '0')
+    }
+  }
 
 }
 </script>
-
 <style>
 
 </style>

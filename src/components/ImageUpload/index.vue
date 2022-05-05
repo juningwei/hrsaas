5579c91e690691437a6853bdfca9624bf9cbc0cd
juningwei <289704828@qq.com>
2022-05-05 13:48:42 +0800

    上传图片之前的检查

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -6,6 +6,8 @@
       action="#"
       :on-preview="preview"
       :on-remove="handleRemove"
+      :on-change="changeFile"
+      :before-upload="beforeUpload"
       :file-list="fileList"
       :class="{disabled: fileComputed }"
     >
@@ -47,6 +49,22 @@ export default {
     //   将原来的文件给排除掉了 剩下的就是最新的数组了
       this.fileList = this.fileList.filter(item => item.uid !== file.uid)
     },
+    beforeUpload(file) {
+      // 要开始做文件上传的检查了
+      // 文件类型 文件大小
+      const types = ['image/jpeg', 'image/gif', 'image/bmp', 'image/png']
+      if (!types.includes(file.type)) {
+        this.$message.error('上传图片只能是 JPG、GIF、BMP、PNG 格式!')
+        return false
+      }
+      //  检查大小
+      const maxSize = 5 * 1024 * 1024
+      if (maxSize < file.size) {
+        this.$message.error('图片大小最大不能超过5M')
+        return false
+      }
+      return true
+    }
   },
 };
 </script>

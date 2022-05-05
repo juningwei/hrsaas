d62e692bfb30472ce42d9aeaf91ce6d8f3a6fb3f
juningwei <289704828@qq.com>
2022-05-05 19:03:50 +0800

    给员工分配角色

--- src/views/employees/components/assign-role.vue
+++ src/views/employees/components/assign-role.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-dialog title="分配角色" :visible="showRoleDialog">
+  <el-dialog title="分配角色" :visible="showRoleDialog" @close="btnCancel">
     <!-- el-checkbox-group选中的是 当前用户所拥有的角色  需要绑定 当前用户拥有的角色-->
     <el-checkbox-group v-model="roleIds">
       <!-- 选项 -->
@@ -11,8 +11,8 @@
     </el-checkbox-group>
     <el-row slot="footer" type="flex" justify="center">
       <el-col :span="6">
-        <el-button type="primary" size="small">确定</el-button>
-        <el-button size="small">取消</el-button>
+        <el-button type="primary" size="small" @click="btnOK">确定</el-button>
+        <el-button size="small" @click="btnCancel">取消</el-button>
       </el-col>
     </el-row>
   </el-dialog>
@@ -21,6 +21,7 @@
 <script>
 import { getRoleList } from '@/api/setting'
 import { getUserDetailById } from '@/api/user'
+import { assignRoles } from '@/api/employees'
 
 export default {
   props: {
@@ -52,6 +53,15 @@ export default {
     async getUserDetailById(id) {
       const { roleIds } = await getUserDetailById(id)
       this.roleIds = roleIds // 赋值本用户的角色
+    },
+    async btnOK() {
+      await assignRoles({ id: this.userId, roleIds: this.roleIds })
+      //   关闭窗体
+      this.$emit('update:showRoleDialog', false)
+    },
+    btnCancel() {
+      this.roleIds = [] // 清空原来的数组
+      this.$emit('update:showRoleDialog', false)
     }
   }
 }

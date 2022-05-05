518ef16f4cc41c262e6973cd8db068977c13e671
juningwei <289704828@qq.com>
2022-05-05 12:58:23 +0800

    点击图片预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <el-upload list-type="picture-card">
-     <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload
+      list-type="picture-card"
+      :limit="1"
+      action="#"
+      :on-preview="preview"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog title="图片" :visible.sync="showDialog">
+      <img :src="imgUrl" style="width: 100%" alt="" />
+    </el-dialog>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      fileList: [
+        {
+          url: "http://i1.shaodiyejin.com/uploads/tu/201910/9999/457d40101f.jpg",
+        },
+      ], // 图片地址设置为数组
+      showDialog: false, // 控制显示弹层
+      imgUrl: ''
+    };
+  },
+  methods: {
+    preview(file) {
+      console.log(file);
+      this.imgUrl = file.url
+      this.showDialog = true
+    },
+  },
+};
+</script>

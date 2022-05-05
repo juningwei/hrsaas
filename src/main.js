64887635a6d383d08f16abaa186c936d86dacb1c
juningwei <289704828@qq.com>
2022-05-05 16:26:04 +0800

    利用v-print打印

--- src/main.js
+++ src/main.js
@@ -45,6 +45,10 @@ Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
+import Print from 'vue-print-nb'
+Vue.use(Print);
+
+
 Vue.config.productionTip = false
 
 new Vue({

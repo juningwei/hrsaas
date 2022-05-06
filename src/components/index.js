00292d80f15ce87663ae569f9def67ae4898fffd
juningwei <289704828@qq.com>
2022-05-06 19:48:01 +0800

    全屏

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import ScreenFull from './ScreenFull'
 
 export default {
   install(Vue) {
@@ -9,5 +10,6 @@ export default {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
     Vue.component('ImageUpload', ImageUpload)
+    Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
   }
 }

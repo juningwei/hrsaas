257f98897d7b6493735734c0fe7516a89f8596ab
juningwei <289704828@qq.com>
2022-05-06 22:34:59 +0800

    多页签

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import ImageUpload from './ImageUpload'
 import ScreenFull from './ScreenFull'
 import ThemePicker from './ThemePicker'
 import Lang from './Lang'
+import TagsView from './TagsView'
 
 export default {
   install(Vue) {
@@ -15,5 +16,7 @@ export default {
     Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
     Vue.component('ThemePicker', ThemePicker)
     Vue.component('Lang', Lang)
+    Vue.component('TagsView', TagsView)
+
   }
 }
